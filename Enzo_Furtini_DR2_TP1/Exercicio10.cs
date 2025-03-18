cae042bf717959ace2c68412b357351ff0bf2254
Glitch <enfurtini@gmail.com>
2025-03-17 22:48:47 -0300

    Exercício 10

---
+++ Enzo_Furtini_DR2_TP1/Exercicio10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enzo_Furtini_DR2_TP1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string nome = "Carlos"; 
+			int idade = 25;        
+
+			Console.WriteLine($"Meu nome é {nome} e eu tenho {idade} anos.");
+		}
+	}
+
+}
